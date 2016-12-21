f26e915141d9cfe31b9e8af91b3acebe0518d533
dECoDER <dECoDER>
2016-12-21 19:26:24 +0530

    first commit

---
+++ sid_shareit_plugin.php
@@ -0,0 +1,10 @@
+<?php 
+    /*
+    Plugin Name: shareIt.js – Social Content Locker jQuery Plugin 
+    Plugin URI: http://www.github.com
+    Description: shareIt.js is a jQuery Social Content Locker Plugin. With shareIt, you can lock anything in your content and the content will be automatically unlocked when user share it on Social Media.
+    Author: Sidharth Menon
+    Version: 1.0
+    Author URI: http://www.orangecreative.net
+    */
+?>
